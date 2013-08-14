8adc8d806e62dea9bf01679ac0b8ecb1dba74e7b
Denis Rizov <d.b.rizov@gmail.com>
2013-08-14 15:31:11 -0700

    Entity models added
    User + Message entity models

---
+++ WebChatApplication/WebChat.Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace WebChat.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string FileUrl { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public int SenderId { get; set; }
+
+        public int RetrieverId { get; set; }
+
+        public virtual User Sender { get; set; }
+
+        public virtual User Retriever { get; set; }
+    }
+}
---
+++ WebChatApplication/WebChat.Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace WebChat.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string NickName { get; set; }
+
+        public string HashedPass { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string SessionKey { get; set; }
+    }
+}
